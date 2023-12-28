e99d1c620716ee815610775f2f205edc37f95a97
Fernando González Gaete <83770169+FerkoxGG@users.noreply.github.com>
2023-12-27 22:25:43 -0300

    Update routing paths to use kebab-case

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
-import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
-import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
-import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { ByCountryPageComponent } from "./pages/by-country-page/by-country-page.component";
+import { ByRegionPageComponent } from "./pages/by-region-page/by-region-page.component";
+import { CountryPageComponent } from "./pages/country-page/country-page.component";
 
 const routes: Routes = [
   {
-    path: 'by-capital',
-    component: ByCapitalPageComponent,
+    path: "by-capital",
+    component: ByCapitalPageComponent
   },
   {
-    path: 'by-country',
-    component: ByCountryPageComponent,
+    path: "by-country",
+    component: ByCountryPageComponent
   },
   {
-    path: 'by-region',
-    component: ByRegionPageComponent,
+    path: "by-region",
+    component: ByRegionPageComponent
   },
   {
-    path: 'by/:id',
-    component: CountryPageComponent,
+    path: "by/:id",
+    component: CountryPageComponent
   },
   {
     path: "**",
@@ -28,17 +28,12 @@ const routes: Routes = [
   },
   {
     path: "",
-    component: ByCapitalPageComponent,
+    component: ByCapitalPageComponent
   }
-]
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [
-    RouterModule
-  ],
-
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class CountriesRoutingModule { }
+export class CountriesRoutingModule {}
